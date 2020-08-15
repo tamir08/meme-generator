1c159292b8fa57b0eba3dc89d0224da6f413d8b2
Tamir K <tamirkuk@gmail.com>
2020-08-15 23:23:20 +0300

    adjust canvas to screen

--- js/main-controller.js
+++ js/main-controller.js
@@ -13,6 +13,7 @@ function downloadCanvas(elLink) {
 function onImageChoose(imgId) {
    document.querySelector('.main-home').classList.toggle('hide')
    document.querySelector('.main-editor').classList.toggle('hide')
+   resizeCanvas();
    createNewMeme(imgId);
    drawCanvasImage()
 }
@@ -25,4 +26,13 @@ function renderGallery() {
 
 
 
+function resizeCanvas() {
+   const elBox= document.querySelector('.canvas-box')
+   const xSize = elBox.offsetWidth;
+   const ySize = elBox.offsetHeight;
+   console.log(xSize,ySize);
+   let elCanvas = document.getElementById('imgCanvas');;
+   elCanvas.hight=ySize;
+   elCanvas.width=xSize;
+}
 
